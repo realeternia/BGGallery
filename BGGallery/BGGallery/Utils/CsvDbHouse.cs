d7db0e8e7d68be4ab6c46342433faa313d4b3faf
narlon <narlonzjh@163.com>
2024-03-01 22:59:39 +0800

    bg optimise merge

---
+++ BGGallery/BGGallery/Utils/CsvDbHouse.cs
@@ -0,0 +1,14 @@
+﻿namespace BGGallery.Utils
+{
+    class CsvDbHouse
+    {
+        public static CsvDbHouse Instance = new CsvDbHouse();
+
+        public CsvDb DB { get; private set; }
+
+        public CsvDbHouse()
+        {
+            DB = CsvDb.Create("rolelist");
+        }
+    }
+}
